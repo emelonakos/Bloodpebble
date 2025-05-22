411274846ddf68595b9d7f4484c2d3adbbf88958
Nicholas Toby <cheesasaurus@gmail.com>
2025-05-22 08:42:56 -0500

    remove unnecessary extensions

--- API/VExtensions.cs
+++ API/VExtensions.cs
@@ -20,18 +20,4 @@ public static class VExtensions
         ServerChatUtils.SendSystemMessageToClient(VWorld.Server.EntityManager, user, ref messageString512Bytes);
     }
 
-    /// <summary>
-    /// Modify the given component on the given entity. The argument is passed
-    /// as a reference, so it can be modified in place. The resulting struct
-    /// is written back to the entity.
-    /// </summary>
-    public static void WithComponentData<T>(this Entity entity, ActionRef<T> action)
-        where T : struct
-    {
-        var component = VWorld.Game.EntityManager.GetComponentData<T>(entity);
-        action(ref component);
-        VWorld.Game.EntityManager.SetComponentData<T>(entity, component);
-    }
-
-    public delegate void ActionRef<T>(ref T item);
 }
